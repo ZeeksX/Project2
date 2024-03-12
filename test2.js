6206dfc3a55f84994fbee5db4c8b0a44e225488f
ZeeksX <155986996+ZeeksX@users.noreply.github.com>
2024-03-12 08:05:04 +0100

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,5 +1,15 @@
  const btn = document.querySelector('button');
+ const song = document.getElementById('change');
+ const cancel = document.getElementById('cancel');
 btn.addEventListener('click', function () {
         alert('Payment made sucessfully');
+        window.reload();
+})
+song.addEventListener('click', function(){
+        alert('Plan Changed Successfully')
+        window.reload();
+})
+cancel.addEventListener('click', function(){
+        alert('Order Cancelled')
         window.reload();
 })
